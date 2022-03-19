96f3a982c019faea2360cab410a37a14907eb2aa
dsant041 <dsant041@uottawa.ca>
2022-03-18 22:27:52 -0400

    Time: 0 ms (100.00%), Space: 45.5 MB (59.68%) - LeetHub

---
+++ 141-linked-list-cycle/141-linked-list-cycle.java
@@ -0,0 +1,43 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        
+        if (head == null)
+            return false;
+        
+        if (head.next == null)
+            return false;
+        else if (head.next == head)
+            return true;
+        
+        ListNode point = head;
+        
+        
+        
+       
+            
+        while (point != null && point.next != null){
+            
+            head = head.next;
+            point = point.next.next;
+            
+            if (head == point)
+                return true;
+        
+            
+        }
+        
+        return false;
+        
+    }
+}
