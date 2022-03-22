f873106be800de7c5d04a6d6c2711fde303e7859
dsant041 <dsant041@uottawa.ca>
2022-03-21 20:57:35 -0400

    Time: 1 ms (99.78%), Space: 41.8 MB (94.72%) - LeetHub

--- 1-two-sum/1-two-sum.java
+++ 1-two-sum/1-two-sum.java
@@ -4,8 +4,23 @@ class Solution {
         if (nums.length == 2)
             return new int[]{0,1};
         
+        Map<Integer,Integer> map = new HashMap<>();
         
+        //key = nums[i]
+        //value = i
         
+        for (int i=0;i<nums.length;i++){
+            
+            if (map.containsKey(target-nums[i]))
+                return new int[]{i,map.get(target-nums[i])};
+            else
+                map.put(nums[i],i);
+            
+        }
+        
+        return null;
+        
+        /*
         for (int i=0;i<nums.length-1;i++){
             for (int j=i+1; j<nums.length;j++){
                 
@@ -15,6 +30,9 @@ class Solution {
         }
         
         return null;
+        */
+                
+        
         
     }
 }
