e05e34e2f41a747085363d8feb7455e160dfaca1
dsant041 <dsant041@uottawa.ca>
2022-04-14 13:06:31 -0400

    Time: 6 ms (56.65%), Space: 42.4 MB (90.21%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.java
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.java
@@ -7,9 +7,28 @@ class Solution {
         if (k>nums.length || k<1)
             return 0;
         
+        
+        // sorting then returning length-k
+        /*
         Arrays.sort(nums);
         
         return nums[nums.length-k];
+        */
+        
+        PriorityQueue<Integer> maxheap = new PriorityQueue<Integer>(Collections.reverseOrder());
+        
+        for (int i=0; i<nums.length; i++){
+            maxheap.add(nums[i]);
+        }
+        
+        while (k>1){
+            
+            maxheap.poll();
+            k--;
+            
+        }
+        
+        return maxheap.poll();
         
     }
 }
