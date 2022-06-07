ca75dc45fb97472d9e7d1d69bfd0a1f35e25a22b
dsant041 <dsant041@uottawa.ca>
2022-06-07 19:25:11 -0400

    Time: 0 ms (100.00%), Space: 43.2 MB (14.22%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.java
+++ 88-merge-sorted-array/88-merge-sorted-array.java
@@ -2,36 +2,36 @@ class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
         
         // numbers end of each array
-        int nums1end = m-1;
-        int nums2end = n-1;
+        m--;
+        n--;
         
         // true end of nums1
         int end = nums1.length-1;
         
         while (end >= 0){
             
-            if (nums1end == -1){
-                nums1[end] = nums2[nums2end];
+            if (m == -1){
+                nums1[end] = nums2[n];
                 end--;
-                nums2end--;
+                n--;
             }
-            else if (nums2end == -1){
-                nums1[end] = nums1[nums1end];
+            else if (n == -1){
+                nums1[end] = nums1[m];
                 end--;
-                nums1end--;
+                m--;
             }
             
             else{
                 
-                if (nums1[nums1end] > nums2[nums2end]){
-                    nums1[end] = nums1[nums1end];
+                if (nums1[m] > nums2[n]){
+                    nums1[end] = nums1[m];
                     end--;
-                    nums1end--;
+                    m--;
                 }
                 else{
-                    nums1[end] = nums2[nums2end];
+                    nums1[end] = nums2[n];
                     end--;
-                    nums2end--;
+                    n--;
                 }
                 
             }
