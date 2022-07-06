3a137523543af44bb7c943bef7ae3901f4583e58
dsant041 <dsant041@uottawa.ca>
2022-07-05 21:09:26 -0400

    Time: 27 ms (57.79%), Space: 13.5 MB (35.69%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.py
+++ 509-fibonacci-number/509-fibonacci-number.py
@@ -4,10 +4,13 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        
+        if n <=1:
+            return n
         f = 0
         f1 = 0
         f2 = 1
+        
+        
         
         for i in range(2,n+1):
             
@@ -15,7 +18,5 @@ class Solution(object):
             f1 = f2
             f2 = f
             
-        if n <=1:
-            return n
-        else:
-            return f
+        
+        return f
